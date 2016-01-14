94efc534e5efa12b2947143005dca612d8730450
Vitaly Dryukov <miniwe@mail.ru>
2016-01-15 00:30:20 +0100

    add meteor package js

---
+++ package.js
@@ -0,0 +1,31 @@
+Package.describe({
+  name: 'miniwe:daemonite-material',
+  version: '1.2.1',
+  summary: 'HTML5 UI design based on Google Material',
+  git: 'https://github.com/Miniwe/material.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.use([
+    'jquery'
+  ], ['client']);
+
+  api.use([
+    'fourseven:scss'
+  ], ['client', 'server']);
+
+  api.addAssets([
+    'css/fonts/MaterialIcons-Regular.eot',
+    'css/fonts/MaterialIcons-Regular.ijmap',
+    'css/fonts/MaterialIcons-Regular.ttf',
+    'css/fonts/MaterialIcons-Regular.woff',
+    'css/fonts/MaterialIcons-Regular.woff2',
+  ], ['client']);
+
+  api.addFiles([
+    'js/base.js',
+    'assets/sass/base.scss'
+  ], ['client']);
+
+});
